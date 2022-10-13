64f369fcfc16e354d9d17b6f7a552911af1a2c1c
opuzayed <zayedopu4933@gmail.com>
2022-10-13 14:33:57 +0600

    phn data loaded

--- iphone-hunter-server/index.js
+++ iphone-hunter-server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const phones = require('./phones.json');
 const port = 5000;
 
 app.get('/', (req, res) => {
@@ -7,8 +8,15 @@ app.get('/', (req, res) => {
 });
 
 app.get('/phones', (req, res) =>{
-  res.send('phon data is comming');  
+  res.send(phones);  
+});
+app.get('/phones/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log('looking for data ', id);
+    const phone = phones.find(ph => ph.id === id) || {};
+    res.send(phone);
 })
 app.listen(port, () =>{
     console.log(`iphone server is running on port : ${port}`);
+    
 });
